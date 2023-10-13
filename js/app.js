a9c95e627a153d9338897a12849f7d39de34065c
Jonathangaleano <143469380+Jonathangaleano@users.noreply.github.com>
2023-10-13 16:52:42 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,123 @@
+
+const headerMenu=document.querySelector('.hm-header');
+
+console.log(headerMenu.offsetTop);
+
+window.addEventListener('scroll',()=>{
+    if(window.pageYOffset > 80){
+        headerMenu.classList.add('header-fixed');
+    }else{
+        headerMenu.classList.remove('header-fixed');
+    }
+})
+
+/*=========================================
+    Tabs
+==========================================*/
+if(document.querySelector('.hm-tabs')){
+
+    const tabLinks=document.querySelectorAll('.hm-tab-link');
+    const tabsContent=document.querySelectorAll('.tabs-content');
+
+    tabLinks[0].classList.add('active');
+
+    if(document.querySelector('.tabs-content')){
+        tabsContent[0].classList.add('tab-active');
+    }
+    
+
+    for (let i = 0; i < tabLinks.length; i++) {
+        
+        tabLinks[i].addEventListener('click',()=>{
+
+            
+            tabLinks.forEach((tab) => tab.classList.remove('active'));
+            tabLinks[i].classList.add('active');
+            
+            tabsContent.forEach((tabCont) => tabCont.classList.remove('tab-active'));
+            tabsContent[i].classList.add('tab-active');
+            
+        });
+        
+    }
+
+}
+
+/*=========================================
+    MENU
+==========================================*/
+
+const menu=document.querySelector('.icon-menu');
+const menuClose=document.querySelector('.cerrar-menu');
+
+menu.addEventListener('click',()=>{
+    document.querySelector('.header-menu-movil').classList.add('active');
+})
+
+menuClose.addEventListener('click',()=>{
+    document.querySelector('.header-menu-movil').classList.remove('active');
+})
+
+/*======================================
+     FORMULARIO
+========================================*/
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    // Recopilar los datos del formulario
+    const formData = new FormData(form);
+
+    // Validación en JavaScript 
+
+    const nombre = formData.get("nombre-completo");
+    const email = formData.get("email");
+    const telefono = formData.get("telefono");
+    const asunto = formData.get("asunto");
+     
+    if (nombre.trim() === "") {
+        alert("Por favor, ingrese un nombre valido.");
+        return;
+    }
+    
+    if(!validateEmail(email)) {
+        alert("Por favor, ingrese una direccion de correo electronico valida.");
+        return;
+    }
+
+    if(!validatePhone(telefono)) {
+        alert("Por favor, ingrese un numero de telefono valido.");
+        return;
+    }
+
+    if(asunto.trim() === "") {
+        alert("Por favor, ingrese un asunto.");
+        return;
+    }
+
+
+    // Enviar datos a través de una solicitud Fetch
+    const response = await fetch("https://formsubmit.co/ajax/API_KEY", {
+        method: "POST",
+        body: formData,
+    });
+
+    if (response.ok) {
+        alert("Mensaje enviado con éxito");
+        form.reset();
+    } else {
+        alert("Ocurrió un error al enviar el mensaje. Inténtalo de nuevo más tarde.");
+    }
+});
+
+
+function validateEmail(email) {
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(email);
+}
+
+function validatePhone(telefono) {
+    return /^\d{10,}$/.test(telefono);
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+## PROYECTO DON BARRIGON
+
+### Aula 23540
+#
+
+##### Garcia Fernando Luis
+##### Zelaya Nahuel Alberto
+##### Galeano Jonathan  Emanuel
+#
+##### TEMATICA: WEB TIENDA FICTICIA DE FAST FOOD
+
+
+- Se trabajó de manera local en grupo previo a subir el inicio del proyecto.
+- Se realizó el index.html en conjunto, luego el mismo se utilizó como plantilla para el resto de las paginas del sitio.
+- falta definir footer
+  

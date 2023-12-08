5ded662bb1dfc675251c88fa532aedda888bc06c
Nahuel Zelaya <nahuelzelaya89@gmail.com>
2023-12-08 12:48:02 -0300

    cambios en productos.html

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
 navbar-light bg-light">
 <div class="container">
+<a class="navbar-brand" href="../templates/productos.html">CRUD</a>
 
-<img src="/img/logo.png" alt="" /><
 
 
 </div>

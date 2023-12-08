e0b7c3a57ca907264fc9de1ef74e866ca0fdabed
Nahuel Zelaya <nahuelzelaya89@gmail.com>
2023-12-08 12:31:33 -0300

    header crud

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
 navbar-light bg-light">
 <div class="container">
-<a class="navbar-brand" href="../templates/productos.html">CRUD</a>
+<div class="hm-logo">
+
+<a href="index.html"><img src="img/logo.png" alt="" /></a></div>
 
 
 

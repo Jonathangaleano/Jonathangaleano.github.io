fb6292afd11b13c581666ebcb7b1959164adb09a
Nahuel Zelaya <nahuelzelaya89@gmail.com>
2023-12-08 12:37:13 -0300

    correccion nav

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
 navbar-light bg-light">
 <div class="container">
-<div class="hm-logo">
 
-<a href="index.html"><img src="../img/logo.png" alt="" /></a></div>
-
+<img src="/img/logo.png" alt="" /><
 
 
 </div>

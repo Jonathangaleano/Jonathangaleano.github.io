c1cd78264208a30da8246488c05423761b1b38f0
Nahuel Zelaya <nahuelzelaya89@gmail.com>
2023-12-08 12:33:49 -0300

    url img

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ navbar-light bg-light">
 <div class="container">
 <div class="hm-logo">
 
-<a href="index.html"><img src="img/logo.png" alt="" /></a></div>
+<a href="index.html"><img src="../img/logo.png" alt="" /></a></div>
 
 
 
